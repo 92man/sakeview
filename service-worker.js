7316d5e621bd1b2a535b471f116305f23c44f98c
92man <kooeyman@gmail.com>
2026-06-11 12:52:24 +0900

    추천사케 비율 조정

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'sakeview-v95';
+const CACHE_VERSION = 'sakeview-v96';
 const IMAGE_CACHE = 'sakeview-images-v1';
 const STATIC_ASSETS = [
   '/',
@@ -8,7 +8,7 @@ const STATIC_ASSETS = [
   '/community.css?v=12',
   '/certification.css?v=2',
   '/flavor-wheel.css?v=2',
-  '/design-refresh.css?v=1',
+  '/design-refresh.css?v=2',
   '/utils.js?v=5',
   '/age-consent.js?v=4',
   '/featured.js?v=3',
